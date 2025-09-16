fe6547ed7e3a6ecb2760a97f840d776e7e1b07f5
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-17 03:23:53 +1000

    remove permute

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -216,9 +216,10 @@ class OnPolicyRunner:
                     obs = self.obs_normalizer(obs)
                     if isinstance(obs, dict) and "image" in obs:
                         image = obs["image"].to(self.device).float() / 255.0  # Normalize to [0,1]
-                        image = image.permute(0, 3, 1, 2)  # Convert to (B, C, H, W)
                         with torch.no_grad():
                             obs = self.alg.policy.encode(image)
+                        assert encoded_obs.ndim == 2,
+
                     if self.privileged_obs_type is not None:
                         privileged_obs = self.privileged_obs_normalizer(
                             infos["observations"][self.privileged_obs_type].to(self.device)
