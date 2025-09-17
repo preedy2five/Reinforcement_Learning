2a78aabb415fe61a68ade96b99250bff1264f549
Maxwell Lay <maxwell.lay@data61.csiro.au>
2025-09-17 13:00:53 +1000

    fix typo

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -217,7 +217,7 @@ class OnPolicyRunner:
                     if isinstance(obs, dict) and "image" in obs:
                         image = obs["image"].to(self.device).float() / 255.0  # Normalize to [0,1]
                         with torch.no_grad():
-                            obs = self.alg.policy.encode(image)\
+                            obs = self.alg.policy.encode(image)
                             print("Encoded obs shape:", obs.shape)
 
 
